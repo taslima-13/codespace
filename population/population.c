146e9d60f64e7298bf3b3b31f11b4754f643198d
taslima-13 <144816324+taslima-13@users.noreply.github.com>
2023-09-19 11:49:02 +0000

    Tue, Sep 19, 2023, 11:49 AM +00:00

--- population/population.c
+++ population/population.c
@@ -22,6 +22,7 @@ int main(void)
     // TODO: Calculate number of years until we reach threshold
     int n = start;
     int years = 0;
+    
     while (n < end)
     {
         n = n + n/3 - n/4;
