cae85be1eefabb9bfab61685a6b764dd2de9be9f
taslima-13 <144816324+taslima-13@users.noreply.github.com>
2023-09-19 09:04:44 +0000

    Tue, Sep 19, 2023, 9:04 AM +00:00

--- population/population.c
+++ population/population.c
@@ -4,10 +4,30 @@
 int main(void)
 {
     // TODO: Prompt for start size
+    int start;
+    do
+    {
+        start = get_int("start size:");
+    }
+    while (start<9);
 
     // TODO: Prompt for end size
+    int end;
+    do
+    {
+        end = get_int("end size:");
+    }
+    while (end<start);
 
     // TODO: Calculate number of years until we reach threshold
+    int n = start;
+    int years = 0;
+    for(int i =0; i < end; years++)
+    {
+        n = n + n/3-n/4;
+    }
+
 
     // TODO: Print number of years
+    printf("years: %i\n", years);
 }
